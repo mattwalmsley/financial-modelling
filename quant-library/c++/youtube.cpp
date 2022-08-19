0a32f5eb7028b0f23af9a86f351428a06751057a
matt <matthewkjrwalmsley@hotmail.co.uk>
2022-08-19 12:58:49 +0100

    Update with array of investment rates.

---
+++ quant-library/c++/youtube.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int slices;
+    cin >> slices;
+    cout
+        << "You have " << slices << " slices" << std::endl;
+}
